8755349b26efc5ef8df047f95f72cbb43b2ec692
AlexeyPetrov <alexey.webmailr@gmail.com>
2018-01-04 21:08:15 +0300

    Variables for save menu items state

--- app/src/main/java/pw/powerhost/smsfilter/MainActivity.java
+++ app/src/main/java/pw/powerhost/smsfilter/MainActivity.java
@@ -1,11 +1,24 @@
 package pw.powerhost.smsfilter;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Settings file name
+     */
+    static final String APP_PREFERENCES = "appsettings";
+    /**
+     * Stored settings param names
+     */
+    static final String APP_PREFERENCES_ACTIVE_FILTER = "active_filter";
+    static final String APP_PREFERENCES_STORE_SMS = "store_sms";
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
