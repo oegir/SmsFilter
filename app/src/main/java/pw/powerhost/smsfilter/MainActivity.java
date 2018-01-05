c0065690fee691f7188be85f9bf1db600a8cde3c
Alexey <alexey_mai@rambler.ru>
2018-01-05 14:56:24 +0300

    Сохранение состояний пунктов меню

--- app/src/main/java/pw/powerhost/smsfilter/MainActivity.java
+++ app/src/main/java/pw/powerhost/smsfilter/MainActivity.java
@@ -1,5 +1,6 @@
 package pw.powerhost.smsfilter;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,28 +13,43 @@ public class MainActivity extends AppCompatActivity {
      * Settings file name
      */
     static final String APP_PREFERENCES = "appsettings";
-    /**
-     * Stored settings param names
-     */
-    static final String APP_PREFERENCES_ACTIVE_FILTER = "active_filter";
-    static final String APP_PREFERENCES_STORE_SMS = "store_sms";
+    private SharedPreferences mSettings;
 
-    private SharedPreferences sharedPreferences;
+    /**
+     * Return string presentation of menu item identifier
+     */
+    private String getMenuItemId(MenuItem item) {
+        return getResources().getResourceName(item.getItemId()).split("\\/")[1];
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Load application preferences
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        // Restore menu items state from settings
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem item = menu.getItem(i);
+            Boolean isChecked = mSettings.getBoolean(getMenuItemId(item), false);
+            item.setChecked(isChecked);
+        }
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Change menu item state
+        item.setChecked(!item.isChecked());
+        // Save menu item new state
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putBoolean(getMenuItemId(item), item.isChecked());
+        editor.apply();
         return true;
     }
 }
